5bf7201658089247b802887be4bb7dabe9accef5
SimoneFelici <felicisimone9@gmail.com>
2024-12-19 21:33:46 +0100

    norm

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,24 +1,24 @@
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *string;
-	int	i;
-	int j;
+	char	*string;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	string = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	while(s1[j] != '\0')
+	while (s1[j] != '\0')
 	{
 		string[i] = s1[j];
 		i++;
 		j++;
 	}
 	j = 0;
-	while(s2[j] != '\0')
+	while (s2[j] != '\0')
 	{
 		string[i] = s2[j];
 		i++;
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,71 +1,71 @@
 #include "libft.h"
 
-static int before(char const *s1, char const *set)
+static int	before(char const *s1, char const *set)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (s1[i])
-    {
-        j = 0;
-        while (set[j])
-        {
-            if (s1[i] == set[j])
-                break;
-            j++;
-        }
-        if (set[j] == '\0')
-            return (i);
-        i++;
-    }
-    return (i);
+	i = 0;
+	while (s1[i])
+	{
+		j = 0;
+		while (set[j])
+		{
+			if (s1[i] == set[j])
+				break ;
+			j++;
+		}
+		if (set[j] == '\0')
+			return (i);
+		i++;
+	}
+	return (i);
 }
 
-static int after(char const *s1, char const *set, int len)
+static int	after(char const *s1, char const *set, int len)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = len - 1;
-    while (i >= 0)
-    {
-        j = 0;
-        while (set[j])
-        {
-            if (s1[i] == set[j])
-                break;
-            j++;
-        }
-        if (set[j] == '\0')
-            return (i);
-        i--;
-    }
-    return (-1);
+	i = len - 1;
+	while (i >= 0)
+	{
+		j = 0;
+		while (set[j])
+		{
+			if (s1[i] == set[j])
+				break ;
+			j++;
+		}
+		if (set[j] == '\0')
+			return (i);
+		i--;
+	}
+	return (-1);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    char *string;
-    int start;
-    int end;
-    int len;
+	char	*string;
+	int		start;
+	int		end;
+	int		len;
 
-    if (!s1 || !set)
-        return (NULL);
-    len = ft_strlen(s1);
-    if (len == 0)
-        return (ft_calloc(1, sizeof(char)));
-    start = before(s1, set);
-    if (start == len)
-    {
-        return (ft_calloc(1, sizeof(char)));
-    }
-    end = after(s1, set, len);
-    if (end < start)
-    {
-        return (ft_calloc(1, sizeof(char)));
-    }
-    string = ft_substr(s1, start, end - start + 1);
-    return (string);
+	if (!s1 || !set)
+		return (NULL);
+	len = ft_strlen(s1);
+	if (len == 0)
+		return (ft_calloc(1, sizeof(char)));
+	start = before(s1, set);
+	if (start == len)
+	{
+		return (ft_calloc(1, sizeof(char)));
+	}
+	end = after(s1, set, len);
+	if (end < start)
+	{
+		return (ft_calloc(1, sizeof(char)));
+	}
+	string = ft_substr(s1, start, end - start + 1);
+	return (string);
 }
