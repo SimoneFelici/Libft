270370c6e0071623244ae1641db5ad0c8ebc8bb8
SimoneFelici <felicisimone9@gmail.com>
2024-12-20 16:30:03 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -1,6 +1,30 @@
 #include "libft.h"
 
+int	count_words(char const *s, char c)
+{
+	int	count;
+	int	w;
+
+	count = 0;
+	w = 0;
+	while(*s)
+	{
+		if (*s != c && count == 0)
+		{
+			w++;
+			count = 1;
+		}
+		else if (*s == c)
+			count = 0;
+		s++;
+
+	}
+	return (w);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int	words;
 
+	words = count_words(s, c);
 }
